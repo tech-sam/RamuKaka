8e27b7141cb3858629dceb98e05e1885107e8d28
tech-sam <sumit.s7325@gmail.com>
2021-02-27 18:28:58 +0530

    quick sort implementation

--- Algorithms/src/sorting/quicksort/QuickSort.java
+++ Algorithms/src/sorting/quicksort/QuickSort.java
@@ -1,4 +1,36 @@
 package sorting.quicksort;
 
+import java.util.Arrays;
+
 public class QuickSort {
+    public static void main(String[] args) {
+        int[] unsortedArray = {44, 2, 33, 12, 45, 1, 9, 13};
+        quickSort(unsortedArray, 0, unsortedArray.length - 1);
+        System.out.println(Arrays.toString(unsortedArray));
+    }
+
+    public static void quickSort(int[] unsortedArray, int start, int end) {
+        if (start < end) {
+            int pivotIndex = partition(unsortedArray, start, end);
+            quickSort(unsortedArray, start, pivotIndex - 1);
+            quickSort(unsortedArray, pivotIndex + 1, end);
+        }
+    }
+
+    public static int partition(int[] unsortedArray, int start, int end) {
+        int pivot = unsortedArray[end];
+        int j = start;
+        int i = j - 1;
+        while (j <= end) {
+
+            if (unsortedArray[j] <= pivot) {
+                i++;
+                int temp = unsortedArray[i];
+                unsortedArray[i] = unsortedArray[j];
+                unsortedArray[j] = temp;
+            }
+            j++;
+        }
+        return i;
+    }
 }
